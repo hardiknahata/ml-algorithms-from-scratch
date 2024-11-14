343ca7dc1a50355ee84b941d8c25bd51565a3597
Hardik Nahata <hardiknahata@gmail.com>
2024-11-13 20:31:42 -0800

    chore: uses numpy array

--- linear-regression/train.py
+++ linear-regression/train.py
@@ -1,8 +1,10 @@
 from linear_regression import LinearRegression
+import numpy as np
+
 
 # Sample data (2 features)
-X = [[1, 2], [2, 3], [4, 5], [3, 5], [5, 8], [6, 8], [7, 10], [8, 12]]
-y = [3, 5, 9, 8, 14, 15, 18, 20]
+X = np.array([[1, 2], [2, 3], [4, 5], [3, 5], [5, 8], [6, 8], [7, 10], [8, 12]])
+y = np.array([3, 5, 9, 8, 14, 15, 18, 20])
 
 # Create an instance of the LinearRegression model
 model = LinearRegression(learning_rate=0.01, iterations=1000)
